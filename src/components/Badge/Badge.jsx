94e552b3f88d2f4ef38367649407248072cc7272
Asiful Ahsan <asifulahsan@outlook.com>
2025-06-27 17:11:27 +0600

    Adjusted UI themes

--- src/components/Badge/Badge.jsx
+++ src/components/Badge/Badge.jsx
@@ -4,7 +4,7 @@ import { Zap } from "lucide-react";
 function Badge() {
   return (
     <div>
-      <div className="flex gap-2 border-2 border-[#CDD5DC] bg-gray-200 px-6 py-2 rounded-xl cursor-pointer">
+      <div className="flex gap-2 border-2 border-[#CDD5DC] bg-gray-200 px-6 py-2 rounded-xl cursor-text">
         <Zap color="black" />
         <p className="text-black">Trusted by 10,000+ businesses worldwide</p>
       </div>
