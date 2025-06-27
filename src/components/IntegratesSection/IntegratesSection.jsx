83f59e277aba523426f87255e82460fc8898bcde
Asiful Ahsan <asifulahsan@outlook.com>
2025-06-27 21:18:04 +0600

    Configured tools section

--- src/components/IntegratesSection/IntegratesSection.jsx
+++ src/components/IntegratesSection/IntegratesSection.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "../IntegratesSection/IntegratesSection.module.css";
+import { Brackets, Landmark, BriefcaseBusiness } from "lucide-react";
 
 function IntegratesSection() {
   return (
@@ -89,6 +90,35 @@ function IntegratesSection() {
           <div className={styles.card}>
             <p className={styles.cardText}>Google Workspace</p>
           </div>
+        </div>
+      </div>
+      <div className={styles.prosSection}>
+        <div className={styles.prosDiv}>
+          <div className={styles.prosIconDiv}>
+            <Landmark size={24} />
+          </div>
+          <p className={styles.prosHeading}>Banking & Payments</p>
+          <p className={styles.prosText}>
+            Connect with 50+ banks and payment processors
+          </p>
+        </div>
+        <div className={styles.prosDiv}>
+          <div className={styles.prosIconDiv}>
+            <BriefcaseBusiness size={24} />
+          </div>
+          <p className={styles.prosHeading}>E-commerce & CRM</p>
+          <p className={styles.prosText}>
+            Sync with major e-commerce platforms and CRMs
+          </p>
+        </div>
+        <div className={styles.prosDiv}>
+          <div className={styles.prosIconDiv}>
+            <Brackets size={24} />
+          </div>
+          <p className={styles.prosHeading}>Productivity Tools</p>
+          <p className={styles.prosText}>
+            Integrate with team collaboration software
+          </p>
         </div>
       </div>
     </div>
