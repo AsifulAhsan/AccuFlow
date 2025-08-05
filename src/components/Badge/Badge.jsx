fe1bf8004d5b2c576581e6f0b5ea5d27c97af685
Asiful Ahsan <asifulahsan@outlook.com>
2025-08-05 17:39:48 +0600

    Adjusted Badge module for mobile

--- src/components/Badge/Badge.jsx
+++ src/components/Badge/Badge.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Zap } from "lucide-react";
+import styles from "../Badge/Badge.module.css";
 
 function Badge() {
   return (
-    <div>
-      <div className="flex gap-2 border-2 border-[#CDD5DC] bg-gray-200 px-6 py-2 rounded-xl cursor-text">
-        <Zap color="black" />
-        <p className="text-black">Trusted by 10,000+ businesses worldwide</p>
-      </div>
+    <div className={styles.mainDiv}>
+      <Zap color="white" />
+      <p className={styles.badgeText}>
+        Trusted by 10,000+ businesses worldwide
+      </p>
     </div>
   );
 }
