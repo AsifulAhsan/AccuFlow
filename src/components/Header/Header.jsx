11a94322a2dde16c417b10010ab824e934bcc844
Asiful Ahsan <asifulahsan@outlook.com>
2025-08-05 12:46:22 +0600

    Updating navbar responsiveness

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,36 @@
 import React from "react";
 import styles from "./Header.module.css";
+import { useState, useEffect } from "react";
 
 function Header() {
+  const [dimensions, setDimensions] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+
+  useEffect(() => {
+    const handleResize = () => {
+      setDimensions({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+
+    window.addEventListener("resize", handleResize);
+    handleResize(); // Set initial dimensions
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
     <div className={styles.mainDiv}>
       <header className={styles.header}>
         <div className={styles.logoDiv}>
-            <img src="./finext-icon.png" alt="finext logo" className="size-8"/>
+          <img src="./finext-icon.png" alt="finext logo" className="size-8" />
           <h1 className={styles.navTitle}>AccuFlow</h1>
+          <p className={styles.dimension}>
+            {dimensions.width} x {dimensions.height}
+          </p>
         </div>
         <div className={styles.navLinks}>
           <p className={styles.navItems}>Features</p>
